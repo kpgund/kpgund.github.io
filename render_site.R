b74a679a5eeb581938bc2185127162e71de20449
kpgund <kpgund@gmail.com>
2022-09-22 13:05:06 -0400

    Updating hero-image to resize depending on screen width. Updated second line in hero-image

--- render_site.R
+++ render_site.R
@@ -1,4 +1,10 @@
-# install.packages("rmarkdown")
+# install.packages("include_graphics")
 library(rmarkdown)
+library(knitr)
 
 rmarkdown::render_site(encoding = "UTF-8")
+
+
+## sources:
+## https://rstudio4edu.github.io/rstudio4edu-book/rmd-fancy.html#anatomy-of-a-fancy-r-markdown-page
+## http://jsfiddle.net/KQFBb/1/
